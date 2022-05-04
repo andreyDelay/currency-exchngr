e9f65c3b784b40b8b96db33aa1b7f6803b0a141e
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-04 10:46:54 +0300

    initial commit

---
+++ src/main/java/com/andrey/currencyexchgr/CurrencyExchgrApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.currencyexchgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/currencyexchgr/CurrencyExchgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.currencyexchgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
