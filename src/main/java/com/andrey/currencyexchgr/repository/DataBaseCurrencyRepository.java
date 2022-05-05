e8150909a2931c4f5cb4c2b76450fbc9ce727d75
andreyDelay <frowzygleb@gmail.com>
2022-05-05 19:37:03 +0300

    CRUD repository now in use

--- src/main/java/com/andrey/currencyexchgr/repository/DataBaseCurrencyRepository.java
+++ src/main/java/com/andrey/currencyexchgr/repository/DataBaseCurrencyRepository.java
@@ -1,11 +1,11 @@
 package com.andrey.currencyexchgr.repository;
 
 import com.andrey.currencyexchgr.model.CurrencyRate;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface DataBaseCurrencyRepository extends JpaRepository<CurrencyRate, String> {
+public interface DataBaseCurrencyRepository extends CrudRepository<CurrencyRate, String> {
 
 	Optional<CurrencyRate> findByCharCode(String charCode);
 
