502df054da6568a1348e7f729f7fbfc5d5b2d2d8
andreyDelay <frowzygleb@gmail.com>
2022-05-06 00:31:22 +0300

    methods for new API added

--- src/main/java/com/andrey/currencyexchgr/service/CurrencyService.java
+++ src/main/java/com/andrey/currencyexchgr/service/CurrencyService.java
@@ -1,10 +1,23 @@
 package com.andrey.currencyexchgr.service;
 
-import com.andrey.currencyexchgr.model.CurrencyRate;
+import com.andrey.currencyexchgr.dto.CurrencyRateDto;
+import com.andrey.currencyexchgr.dto.CurrencyRatesDto;
+import com.andrey.currencyexchgr.service.model.CurrencyRate;
 
 import java.util.Optional;
 
 public interface CurrencyService {
+
     Optional<CurrencyRate> getCurrencyRateByCode(String currencyCoe);
+
+    void save(CurrencyRateDto currencyRate);
+
+    CurrencyRateDto findById(String charCode);
+
+    CurrencyRatesDto findAll();
+
+    void delete(String charCode);
+
+    CurrencyRateDto update(CurrencyRateDto currencyRateDto);
 }
 
--- src/main/java/com/andrey/currencyexchgr/service/impl/DataBaseCurrencyServiceImpl.java
+++ src/main/java/com/andrey/currencyexchgr/service/impl/DataBaseCurrencyServiceImpl.java
@@ -1,11 +1,15 @@
 package com.andrey.currencyexchgr.service.impl;
 
-import com.andrey.currencyexchgr.model.CurrencyRate;
+import com.andrey.currencyexchgr.dto.CurrencyRateDto;
+import com.andrey.currencyexchgr.dto.CurrencyRatesDto;
+import com.andrey.currencyexchgr.service.model.CurrencyRate;
 import com.andrey.currencyexchgr.repository.DataBaseCurrencyRepository;
 import com.andrey.currencyexchgr.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service("DB-oriented-service")
@@ -18,4 +22,52 @@ public class DataBaseCurrencyServiceImpl implements CurrencyService {
     public Optional<CurrencyRate> getCurrencyRateByCode(String currencyCoe) {
         return repository.findByCharCode(currencyCoe);
     }
+
+    @Override
+    public void save(CurrencyRateDto currencyRateDto) {
+        repository.save(currencyRateDto.toCurrencyRate());
+    }
+
+    @Override
+    public CurrencyRateDto findById(String charCode) {
+        Optional<CurrencyRate> byCharCode = repository.findByCharCode(charCode);
+        return repository.findByCharCode(charCode)
+                .map(response ->
+                        CurrencyRateDto.builder()
+                        .charCode(response.getCharCode())
+                        .value(response.getValue())
+                        .build())
+                .orElseThrow(() -> new RuntimeException("Not found"));
+    }
+
+    @Override
+    public CurrencyRatesDto findAll() {
+        List<CurrencyRateDto> currencyRateDtoList = new ArrayList<>();
+        repository.findAll().forEach(value ->
+                currencyRateDtoList.add(
+                        CurrencyRateDto.builder()
+                        .value(value.getValue())
+                        .charCode(value.getCharCode())
+                        .build()));
+        return new CurrencyRatesDto(currencyRateDtoList);
+    }
+
+    @Override
+    public CurrencyRateDto update(CurrencyRateDto currencyRateDto) {
+        Optional<CurrencyRate> byCharCode = repository.findByCharCode(currencyRateDto.getCharCode());
+        CurrencyRate currencyRate = byCharCode
+                .orElseThrow(() -> new RuntimeException("Not found"));
+        currencyRate.setValue(currencyRateDto.getValue());
+        repository.save(currencyRate);
+        return currencyRateDto;
+    }
+
+    @Override
+    public void delete(String charCode) {
+        Optional<CurrencyRate> targetCurrency = repository.findByCharCode(charCode);
+        CurrencyRate currencyRate = targetCurrency
+                .orElseThrow(() -> new RuntimeException("Not found"));
+        repository.delete(currencyRate);
+    }
+
 }
