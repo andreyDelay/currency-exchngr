1cac8126e443b17cefa1138756b6cf4dd98a2836
andreyDelay <frowzygleb@gmail.com>
2022-05-06 00:35:01 +0300

    constraints added to fields

--- src/main/java/com/andrey/currencyexchgr/dto/CurrencyRateDto.java
+++ src/main/java/com/andrey/currencyexchgr/dto/CurrencyRateDto.java
@@ -4,10 +4,16 @@ import com.andrey.currencyexchgr.service.model.CurrencyRate;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 @Value
 @Builder
 public class CurrencyRateDto {
+
+    @Pattern(regexp = "[A-Z]{3}")
     private String charCode;
+    @Min(1)
     private double value;
 
     public CurrencyRate toCurrencyRate() {
