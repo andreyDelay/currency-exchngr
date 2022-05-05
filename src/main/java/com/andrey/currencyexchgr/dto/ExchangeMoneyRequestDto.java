812628f2c3a44d0006aade320028e67fa8d525d2
andreyDelay <frowzygleb@gmail.com>
2022-05-05 17:34:52 +0300

    validation constraints added

--- src/main/java/com/andrey/currencyexchgr/dto/ExchangeMoneyRequestDto.java
+++ src/main/java/com/andrey/currencyexchgr/dto/ExchangeMoneyRequestDto.java
@@ -2,8 +2,13 @@ package com.andrey.currencyexchgr.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class ExchangeMoneyRequestDto {
+	@Min(1)
 	private double rubBalance;
+	@Pattern(regexp = "[A-Z]{3}")
 	private String targetCurrencyCode;
 }
