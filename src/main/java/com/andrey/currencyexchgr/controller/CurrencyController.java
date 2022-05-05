9d6ec1efd8c51e1be67ef7eecc4442fad42662b3
andreyDelay <frowzygleb@gmail.com>
2022-05-05 17:42:46 +0300

    response entity removed, status fixed

--- src/main/java/com/andrey/currencyexchgr/controller/CurrencyController.java
+++ src/main/java/com/andrey/currencyexchgr/controller/CurrencyController.java
@@ -5,7 +5,6 @@ import com.andrey.currencyexchgr.dto.ExchangeMoneyRequestDto;
 import com.andrey.currencyexchgr.service.ExchangeCurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,9 +17,8 @@ public class CurrencyController {
     private final ExchangeCurrencyService exchangeService;
 
     @PostMapping(path = "convert/", consumes = "application/json")
-    public ResponseEntity<ConvertedCurrencyDto> convert(
-            @Valid @RequestBody ExchangeMoneyRequestDto exchangeMoneyRequestDto) {
-        ConvertedCurrencyDto convertedCurrencyDto = exchangeService.exchangeCurrency(exchangeMoneyRequestDto);
-        return new ResponseEntity<>(convertedCurrencyDto, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public ConvertedCurrencyDto convert(@Valid @RequestBody ExchangeMoneyRequestDto exchangeMoneyRequestDto) {
+        return exchangeService.exchangeCurrency(exchangeMoneyRequestDto);
     }
 }
