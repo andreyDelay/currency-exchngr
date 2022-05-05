ddb4c19e619476bc369a3c202129954f261c65bc
andreyDelay <frowzygleb@gmail.com>
2022-05-05 18:49:32 +0300

    columns names changes

--- src/main/java/com/andrey/currencyexchgr/model/CurrencyRate.java
+++ src/main/java/com/andrey/currencyexchgr/model/CurrencyRate.java
@@ -13,7 +13,8 @@ public class CurrencyRate {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "charcode")
     private String charCode;
-
+    @Column(name = "value")
     private double value;
 }
