12a08686c64291edd654f2c572e1ae6c5afd8fea
andreyDelay <frowzygleb@gmail.com>
2022-05-10 13:03:15 +0300

    add constaint to a field @Min

--- src/main/java/com/andrey/currencyexchgr/dto/UpdateCurrencyDto.java
+++ src/main/java/com/andrey/currencyexchgr/dto/UpdateCurrencyDto.java
@@ -2,7 +2,10 @@ package com.andrey.currencyexchgr.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class UpdateCurrencyDto {
+	@Min(1)
 	private double value;
 }
