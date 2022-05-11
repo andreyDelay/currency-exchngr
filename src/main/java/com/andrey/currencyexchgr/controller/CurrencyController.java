b491aac2c9512fe3de722b1426a9a6180ca08426
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-11 13:57:11 +0300

    minor changes

--- src/main/java/com/andrey/currencyexchgr/controller/CurrencyController.java
+++ src/main/java/com/andrey/currencyexchgr/controller/CurrencyController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
