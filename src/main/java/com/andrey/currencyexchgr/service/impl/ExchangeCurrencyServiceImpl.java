f8e5e8c636ffadb053380b46110e5e099d4d3c2a
andreyDelay <frowzygleb@gmail.com>
2022-05-12 21:59:02 +0300

    desired repository has been declared into @Qualifier

--- src/main/java/com/andrey/currencyexchgr/service/impl/ExchangeCurrencyServiceImpl.java
+++ src/main/java/com/andrey/currencyexchgr/service/impl/ExchangeCurrencyServiceImpl.java
@@ -7,12 +7,14 @@ import com.andrey.currencyexchgr.model.CurrencyRate;
 import com.andrey.currencyexchgr.repository.ApiCurrencyRepository;
 import com.andrey.currencyexchgr.service.ExchangeCurrencyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ExchangeCurrencyServiceImpl implements ExchangeCurrencyService {
 
+    @Qualifier("Postgres-repository")
     private final ApiCurrencyRepository repository;
 
     @Override
