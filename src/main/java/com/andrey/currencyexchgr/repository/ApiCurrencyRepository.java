32f67645902ffb66d348195fcad0b7629bc1bee2
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-12 11:54:03 +0300

    @RequestMapping removed

--- src/main/java/com/andrey/currencyexchgr/repository/ApiCurrencyRepository.java
+++ src/main/java/com/andrey/currencyexchgr/repository/ApiCurrencyRepository.java
@@ -3,15 +3,14 @@ package com.andrey.currencyexchgr.repository;
 import com.andrey.currencyexchgr.config.FeignConfig;
 import com.andrey.currencyexchgr.model.CurrencyRate;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Optional;
 
 @FeignClient(value = "API-repository", url = "${api.url}", configuration = FeignConfig.class)
 public interface ApiCurrencyRepository {
 
-	@RequestMapping(method = RequestMethod.GET, value = "rates/{charCode}")
-	Optional<CurrencyRate> findByCurrencyCode(@PathVariable("charCode") String charCode);
+	@GetMapping("{charCode}")
+	Optional<CurrencyRate> findByCurrencyCode(@PathVariable String charCode);
 }
