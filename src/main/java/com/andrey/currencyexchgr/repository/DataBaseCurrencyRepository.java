e79df5e1641e0c6540a64524b82aed832be43bdb
andreyDelay <frowzygleb@gmail.com>
2022-05-12 21:44:46 +0300

    test bean name replaced

--- src/main/java/com/andrey/currencyexchgr/repository/DataBaseCurrencyRepository.java
+++ src/main/java/com/andrey/currencyexchgr/repository/DataBaseCurrencyRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository("1")
+@Repository("Postgres-repository")
 public interface DataBaseCurrencyRepository extends CrudRepository<CurrencyRate, Long>, ApiCurrencyRepository {
 
 	List<CurrencyRate> findAll();
