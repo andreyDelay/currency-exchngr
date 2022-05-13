dcc299903bf6161bdf4c35f5b393e768907965ab
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-13 09:11:01 +0300

    service layer for exchanger

---
+++ src/main/java/com/andrey/currencyexchgr/service/CurrencyRateSource.java
@@ -0,0 +1,2 @@
+package com.andrey.currencyexchgr.service;public interface CurrencyRateSource {
+}
---
+++ src/main/java/com/andrey/currencyexchgr/service/impl/ApiCurrencyRateSource.java
@@ -0,0 +1,2 @@
+package com.andrey.currencyexchgr.service.impl;public class ApiCurrencyRateSource {
+}
