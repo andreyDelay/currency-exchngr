3abab626ff65157c712eb8448e4c7dd98d99dfc7
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-13 09:11:30 +0300

    decoder moved to feign config

--- src/main/java/com/andrey/currencyexchgr/config/FeignConfig.java
+++ src/main/java/com/andrey/currencyexchgr/config/FeignConfig.java
@@ -1,7 +1,9 @@
 package com.andrey.currencyexchgr.config;
 
-import com.andrey.currencyexchgr.utils.FeignResponseStatusDecoder;
+import com.andrey.currencyexchgr.exception.ApiBadRequestError;
+import com.andrey.currencyexchgr.exception.CurrencyNotFoundException;
 import feign.Logger;
+import feign.Response;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 
@@ -15,5 +17,21 @@ public class FeignConfig {
 	@Bean
 	ErrorDecoder errorDecoder() {
 		return new FeignResponseStatusDecoder();
+	}
+
+	static class FeignResponseStatusDecoder implements ErrorDecoder {
+
+		private ErrorDecoder errorDecoder = new Default();
+
+		@Override
+		public Exception decode(String s, Response response) {
+			switch (response.status()) {
+				case 400 -> throw new ApiBadRequestError("Bad request to API");
+				case 404 -> throw new CurrencyNotFoundException("Target currency code not found by API");
+				default -> {
+					return errorDecoder.decode(s, response);
+				}
+			}
+		}
 	}
 }
