cb62a16937b05c9eff890f2488726e2991d6b759
andrey <andrey.bezrukaviy@teamgetter.com>
2022-05-24 12:39:55 +0300

    currency controller IT finished

--- src/test/java/com/andrey/currencyexchgr/integration/CurrencyControllerTests.java
+++ src/test/java/com/andrey/currencyexchgr/integration/CurrencyControllerTests.java
@@ -2,6 +2,7 @@ package com.andrey.currencyexchgr.integration;
 
 import com.andrey.currencyexchgr.controller.CurrencyController;
 import com.andrey.currencyexchgr.dto.CurrencyRateDto;
+import com.andrey.currencyexchgr.dto.UpdateCurrencyDto;
 import com.andrey.currencyexchgr.exception.CurrencyNotFoundException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,8 +21,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -46,6 +48,15 @@ class CurrencyControllerTests {
 	@Value("${controllers.currencyControllerUrl}")
 	private String currencyControllerUrl;
 
+	@Value("${controllers.notValidCharCode}")
+	private String notValidCharCode;
+
+	@Value("${controllers.notValidRateValue}")
+	private double notValidRateValue;
+
+	@Value("${controllers.notExistingCharCode}")
+	private String notExistingCharCode;
+
 	private CurrencyRateDto createTestCurrencyDto() {
 		return CurrencyRateDto.builder()
 				.charCode(testCharCodeStringValue)
@@ -58,6 +69,11 @@ class CurrencyControllerTests {
 		return objectMapper.writeValueAsString(currencyRateDto);
 	}
 
+	private String getTestCurrencyDtoAsJSON(UpdateCurrencyDto updateCurrencyDto) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(updateCurrencyDto);
+	}
+
 	@Test
 	void contextLoadsControllerAvailable() {
 		assertThat(controller).isNotNull();
@@ -65,7 +81,7 @@ class CurrencyControllerTests {
 	}
 	//POST
 	@Test
-	void shouldSaveCurrency() throws Exception {
+	void postMethodShouldSaveCurrency() throws Exception {
 		CurrencyRateDto testCurrencyRateDto = createTestCurrencyDto();
 		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(testCurrencyRateDto);
 
@@ -81,9 +97,9 @@ class CurrencyControllerTests {
 	}
 
 	@Test
-	void wrongCurrencyCodeShouldFailValidation() throws Exception {
+	void postMethodShouldFailValidationWhenNotValidCharCode() throws Exception {
 		CurrencyRateDto currencyWithWrongCharCode = CurrencyRateDto.builder()
-				.charCode("TEST")
+				.charCode(notValidCharCode)
 				.value(testCharCodeDoubleValue)
 				.build();
 		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(currencyWithWrongCharCode);
@@ -100,10 +116,10 @@ class CurrencyControllerTests {
 	}
 
 	@Test
-	void wrongCurrencyValueShouldFailValidation() throws Exception {
+	void putMethodShouldFailValidationWhenNotValidRateValue() throws Exception {
 		CurrencyRateDto currencyWithWrongCharCode = CurrencyRateDto.builder()
-				.charCode("TBA")
-				.value(0.0)
+				.charCode(testCharCodeStringValue)
+				.value(notValidRateValue)
 				.build();
 		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(currencyWithWrongCharCode);
 
@@ -119,7 +135,7 @@ class CurrencyControllerTests {
 	}
 
 	@Test
-	void shouldReturnApiExceptionWhenSaveIfAlreadyExists() throws Exception {
+	void putMethodShouldReturnApiExceptionWhenSaveIfAlreadyExists() throws Exception {
 		CurrencyRateDto testCurrencyRateDto = createTestCurrencyDto();
 		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(testCurrencyRateDto);
 
@@ -134,7 +150,7 @@ class CurrencyControllerTests {
 	}
 	//GET
 	@Test
-	void shouldReturnCurrencyDtoByCurrencyCode() throws Exception {
+	void getMethodShouldReturnCurrencyDtoByCurrencyCode() throws Exception {
 		mockMvc.perform(get(currencyControllerUrl + "/{charCode}", "USD"))
 				.andDo(print())
 				.andExpect(status().isOk())
@@ -142,9 +158,7 @@ class CurrencyControllerTests {
 	}
 
 	@Test
-	void shouldReturnApiExceptionIfCurrencyCodeNotExist() throws Exception {
-		String notExistingCharCode = "QQQ";
-
+	void getMethodShouldReturnApiExceptionIfCurrencyCodeNotExist() throws Exception {
 		mockMvc.perform(get(currencyControllerUrl + "/{charCode}", notExistingCharCode))
 				.andDo(print())
 				.andExpect(status().isNotFound())
@@ -155,13 +169,121 @@ class CurrencyControllerTests {
 	}
 
 	@Test
-	void getMethodShouldFailValidation() throws Exception {
-		String wrongCharCode = "WRONG CODE";
-
-		mockMvc.perform(get(currencyControllerUrl + "/{charCode}", wrongCharCode))
+	void getMethodShouldFailValidationWhenNotValidCharCode() throws Exception {
+		mockMvc.perform(get(currencyControllerUrl + "/{charCode}", notValidCharCode))
 				.andDo(print())
 				.andExpect(status().isServiceUnavailable())
 				.andExpect(result -> assertThat(result.getResolvedException())
 						.isInstanceOf(ConstraintViolationException.class));
+	}
+
+	@Test
+	void getMethodShouldReturnCollection() throws Exception {
+		mockMvc.perform(get(currencyControllerUrl))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.size()", greaterThanOrEqualTo(0)));
+	}
+	//PUT
+	@Test
+	void putMethodShouldFailConstraintValidation() throws Exception {
+		//when wrong rate value
+		UpdateCurrencyDto currencyDto = new UpdateCurrencyDto();
+		currencyDto.setValue(notValidRateValue);
+		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(currencyDto);
+
+		mockMvc.perform(put(currencyControllerUrl + "/" + testCharCodeStringValue)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonCurrencyRepresentation))
+				.andDo(print())
+				.andExpect(status().isBadRequest())
+				.andExpect(result ->
+						assertThat(result.getResolvedException())
+								.isInstanceOf(MethodArgumentNotValidException.class));
+
+		//when wrong char code
+		currencyDto.setValue(testCharCodeDoubleValue);
+		jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(currencyDto);
+
+		mockMvc.perform(put(currencyControllerUrl + "/" + notValidCharCode)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonCurrencyRepresentation))
+				.andDo(print())
+				.andExpect(status().isServiceUnavailable())
+				.andExpect(result ->
+						assertThat(result.getResolvedException())
+								.isInstanceOf(ConstraintViolationException.class));
+	}
+
+	@Test
+	void putMethodShouldUpdateCurrency() throws Exception {
+		String existingCharCode = "USD";
+		double newRateValue = 111.0;
+		UpdateCurrencyDto updateCurrencyDto = new UpdateCurrencyDto();
+		updateCurrencyDto.setValue(newRateValue);
+		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(updateCurrencyDto);
+
+		mockMvc.perform(put(currencyControllerUrl + "/" + existingCharCode)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonCurrencyRepresentation))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.value", equalTo(newRateValue)))
+				.andExpect(jsonPath("$.charCode", equalTo(existingCharCode)));
+	}
+
+	@Test
+	void putMethodShouldReturnApiExceptionWhenCurrencyCodeNotFound() throws Exception {
+		double newRateValue = 111.0;
+		UpdateCurrencyDto updateCurrencyDto = new UpdateCurrencyDto();
+		updateCurrencyDto.setValue(newRateValue);
+		String jsonCurrencyRepresentation = getTestCurrencyDtoAsJSON(updateCurrencyDto);
+
+		mockMvc.perform(put(currencyControllerUrl + "/" + notExistingCharCode)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(jsonCurrencyRepresentation))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.message",
+						equalTo(String.format("Currency code %s not found", notExistingCharCode))))
+				.andExpect(result ->
+						assertThat(result.getResolvedException())
+								.isInstanceOf(CurrencyNotFoundException.class));
+	}
+
+	//DELETE
+	@Test
+	void deleteMethodShouldDeleteCurrency() throws Exception {
+		String existingCharCode = "EUR";
+
+		mockMvc.perform(delete(currencyControllerUrl + "/" + existingCharCode))
+				.andDo(print())
+				.andExpect(status().isNoContent());
+	}
+
+	@Test
+	void deleteMethodShouldReturnApiExceptionWhenCharCodeNotExist() throws Exception {
+		mockMvc.perform(delete(currencyControllerUrl + "/" + notExistingCharCode))
+				.andDo(print())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andExpect(jsonPath("$.message",
+						equalTo(String.format("Currency code %s not found", notExistingCharCode))))
+				.andExpect(result ->
+						assertThat(result.getResolvedException())
+								.isInstanceOf(CurrencyNotFoundException.class));
+	}
+
+	@Test
+	void deleteMethodShouldFailValidationWhenNotValidCurrencyCode() throws Exception {
+		mockMvc.perform(delete(currencyControllerUrl + "/" + notValidCharCode))
+				.andDo(print())
+				.andExpect(status().isServiceUnavailable())
+				.andExpect(result ->
+						assertThat(result.getResolvedException())
+								.isInstanceOf(ConstraintViolationException.class));
 	}
 }
